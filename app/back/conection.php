43f57ca8bdbf18afac07c87e775e42d1be5203d7
JorgePulgar <159891992+JorgePulgar@users.noreply.github.com>
2025-06-14 16:52:15 +0200

    Update conection.php

--- app/back/conection.php
+++ app/back/conection.php
@@ -1,19 +1,17 @@
 <?php
 
-    $host = "localhost"; // El host de XAMPP. Si tuvieramos un servidor que no fuese local habría que poner su IP o su dominio
-    $dbName = "dnd_tfg"; // Exactamente igual a como está en el XAMPP
-    $userName = "root"; // Usuario por defecto en XAMPP
-    $password = ""; // XAMPP por defecto va sin contraseña, así que mientras estemos en local lo dejamos vacío
+  $host = "mysql.railway.internal";
+  $dbname = "railway";
+  $user = "root";
+  $password = "skrLZnkjnAxoKQfeoqDXvNTQrxezJzGy";
+  $port = 3306;
 
-    try {
-        // Creamos la conexión usando PDO porque es más seguro y flexible. charset=utf8 nos evita problemas con acentos o caracteres especiales
-        $dbConection = new PDO("mysql:host=$host;dbname=$dbName;charset=utf8", $userName, $password);
-        // Configuramos PDO para que lance excepciones en caso de que haya algún error
-        $dbConection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
 
-        // Si conecta no dirá nada. Si no conecta sale la excepción
-    } catch (PDOException $e) {
-        // Usamos die para parar la ejecución del script, y mostramos mensaje de error. En este caso no es necesario el die() porque ya está pasando por la excepción, pero ayuda a recordar usarlo
-        die("Error de conexión: " . $e->getMessage());
-    }
+  try {
+    $pdo = new PDO($dsn, $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  } catch (PDOException $e) {
+    die("Error de conexión a la base de datos: " . $e->getMessage());
+  }
 ?>
